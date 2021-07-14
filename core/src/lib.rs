solana-labs/solana

Re-arrange stuff

core/src/lib.rs
@@ -41,6 +41,8 @@ mod result;
 pub mod retransmit_stage;
 pub mod rewards_recorder_service;
 pub mod rpc;
+pub mod rpc_node_if;
+pub mod rpc_completed_slots_service;
 pub mod rpc_health;
 pub mod rpc_service;
 pub mod sample_performance_service;

sakridge/silk

core/src/lib.rs
@@ -42,6 +42,10 @@ pub mod request_response;
 mod result;
 pub mod retransmit_stage;
 pub mod rewards_recorder_service;
+pub mod rpc;
+pub mod rpc_node_if;
+pub mod rpc_health;
+pub mod rpc_service;
 pub mod sample_performance_service;
 pub mod serve_repair;
 pub mod serve_repair_service;